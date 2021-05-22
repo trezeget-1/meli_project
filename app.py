a7b0ec63fe3b58ef55818b562bbd69cab8f6504c
trezeget-1 <71102019+trezeget-1@users.noreply.github.com>
2021-05-22 16:55:23 -0500

    Setp 5

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import os
 
 app = Flask(__name__)
 
-
-
 @app.route("/")
 def index():
     unknown_id = os.environ.get('API_KEY')
@@ -15,47 +13,37 @@ def index():
 @app.route("/api_shuju_meli")
 def api_meli():
 
-    # db_details = "postgres:karen@localhost:5432/properties_db"
-    # connection_string = f'postgresql://{db_details}'
+    connection_string = os.environ.get('DATABASE_URL', '')
 
-    # ### Create database connection    
-    # engine = create_engine(connection_string, encoding="utf16")
-    # data = pd.read_sql("select * from properties", engine)
+    ### Create database connection    
+    engine = create_engine(connection_string, encoding="utf16")
+    data = pd.read_sql("select * from properties", engine)
 
-#     return(
-#     data
-#     .loc[data['source_of_info'] == 'mercado_libre']
-#     .reset_index()
-#     .drop(['index'], inplace=False, axis=1)
-#     .to_json(force_ascii = True, orient="records")
-# )
-    return jsonify([{
-        "meli":"demo",
-        "views": 100
-    }])
+    return(
+    data
+    .loc[data['source_of_info'] == 'mercado_libre']
+    .reset_index()
+    .drop(['index'], inplace=False, axis=1)
+    .to_json(force_ascii = True, orient="records")
+)
 
 @app.route("/api_shuju_inmuebles24")
 def api_inmuebles24():
 
-    # db_details = "postgres:karen@localhost:5432/properties_db"
-    # connection_string = f'postgresql://{db_details}'
+    connection_string = os.environ.get('DATABASE_URL', '')
 
+    ### Create database connection
+    engine = create_engine(connection_string, encoding="utf16")
+    data = pd.read_sql("select * from properties", engine)
 
-#     ### Create database connection
-#     engine = create_engine(connection_string, encoding="utf16")
-#     data = pd.read_sql("select * from properties", engine)
+    return(
+    data
+    .loc[data['source_of_info'] == 'Inmuebles 24']
+    .reset_index()
+    .drop(['index'], inplace=False, axis=1)
+    .to_json(force_ascii = True, orient="records")
+)
 
-#     return(
-#     data
-#     .loc[data['source_of_info'] == 'Inmuebles 24']
-#     .reset_index()
-#     .drop(['index'], inplace=False, axis=1)
-#     .to_json(force_ascii = True, orient="records")
-# )
-        return jsonify([{
-            "inmuebles24":"demo",
-            "views": 100
-        }])
 
 
 
